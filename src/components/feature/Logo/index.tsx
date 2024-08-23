c92ed92977e876dc50d01a0efbbca1df77637327
razachikyan <razmikachikyan5@gmail.com>
2024-08-23 19:41:54 +0400

    fixed logo theme state issue

--- src/components/feature/Logo/index.tsx
+++ src/components/feature/Logo/index.tsx
@@ -1,16 +1,19 @@
+import { useTheme } from "../../../contexts/ThemeContext";
 import { Image } from "../../shared/image";
 
 import styles from "./styles.module.css";
 
 export const Logo = (): JSX.Element => {
+  const { theme } = useTheme();
   return (
     <div className={styles.logo}>
       <Image
         size={44}
         className={styles.image}
-        src="/icons/logo-light.svg"
+        src={`/icons/logo-${theme}.svg`}
         alt="logo"
       />
+
       <h1 className={styles.name}>Razmik Achikyan</h1>
     </div>
   );

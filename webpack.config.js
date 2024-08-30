c13081f381f4efa5c3de5e7585886a4969d05770
razachikyan <razmikachikyan5@gmail.com>
2024-08-30 11:58:54 +0400

    react definition issue fixed

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
           options: {
             presets: [
               "@babel/preset-env",
-              "@babel/preset-react",
+              ["@babel/preset-react", {"runtime": "automatic"}],
               "@babel/preset-typescript",
             ],
           },

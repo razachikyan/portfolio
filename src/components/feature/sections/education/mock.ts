7abfe9cb1ce931e423012273467c2abe0369c688
Razmik Achikyan <razmikachikyan5@gmail.com>
2024-09-09 19:21:04 +0400

    fixed issues

--- src/components/feature/sections/education/mock.ts
+++ src/components/feature/sections/education/mock.ts
@@ -5,7 +5,7 @@ export const education: Array<IEducation> = [
     title: "National Polytechnic University of Armenia",
     start: new Date("2020-09-01"),
     end: new Date("2022-06-01"),
-    link: "https://polytechnic.am/",
+    link: "https://polytech.am/",
     logo: "/images/npua.png",
     summary: "Bachelor 1-3 course. Learning Information Security.",
     location: "Yerevan",
@@ -14,7 +14,7 @@ export const education: Array<IEducation> = [
     title: "Synopsys Armenia Educational Department",
     start: new Date("2024-09-01"),
     end: new Date("2024-06-01"),
-    link: "https://www.synopsys.com/armenia.html",
+    link: "https://www.synopsys.com/company/contact-synopsys/office-locations/armenia.html",
     logo: "/images/synopsys.jpg",
     summary:
       "Bachelor 3-4 course. Learning EDA (Electronic Design Automation), Computer Science.",
